5cdc85a2f771781f1fed451371ec1a66fae984d7
Delfred353 <Delfred353@gmail.com>
2024-11-18 08:44:04 +0100

    Added class Tower and function in Mob.java

--- game/mobs/Mob.java
+++ game/mobs/Mob.java
@@ -2,11 +2,16 @@ package game.mobs;
 
 public abstract class Mob{
     private String name;
-    private int pv;
+    private int maxHealth;
+    private int currentHealth;
     private int atk;
     private int atkSpeed;
     private int range;
     private Element element;
 
     public Mob(){};
+
+    public int checkVulnerability(Element element){}
+
+    public int attack(Mob target){}
 }
---
+++ game/mobs/Tower.java
@@ -0,0 +1,6 @@
+package game.mobs;
+
+public class Tower extends Mob{
+    private int cost;
+    private int range;
+}
