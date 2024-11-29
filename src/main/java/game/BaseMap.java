f0934c193e8c8ca6871ff52043b78c09618669df
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2024-11-30 00:12:30 +0100

    Adding methods in BaseMap.java
    Added :
       - BaseMap's Constructor
       - drawGameInfos

--- src/main/java/game/BaseMap.java
+++ src/main/java/game/BaseMap.java
@@ -1,9 +1,27 @@
 package game;
 import libs.StdDraw;
+
 public class BaseMap {
-    public static void main(String[] args) {
+    double version;
+    public BaseMap(double ver){
+        this.version = ver;
         StdDraw.setCanvasSize(1080,720);
-        StdDraw.setPenRadius(0.005);
+        StdDraw.setTitle("Tower Defense VideCoq_Merrer V" + version);
+    }
+
+    public void drawGameInfos(int[] level, int[] wave){
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.rectangle(0.9, 0.987, 0.10, 0.013);
+    }
+    
+    public static void main(String[] args) {
+        //Setup de la fenetre
+        double version = 0;
+        StdDraw.setCanvasSize(1080,720);
+        StdDraw.setTitle("Tower Defense VideCoq_Merrer V" + version);
+        
+        //Setup du crayon
+        StdDraw.setPenRadius(0.003);
 
         //Dessine la zone Info Jeu
         StdDraw.setPenColor(StdDraw.RED);
@@ -15,7 +33,9 @@ public class BaseMap {
         StdDraw.setPenColor(StdDraw.BLUE);
         StdDraw.rectangle(0.9, 0.46, 0.1, 0.46);
         //Dessine la zone Carte
-        StdDraw.setPenColor(StdDraw.PURPLE);
+        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.filledRectangle(0.4,0.5,0.4,0.5);
+        StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.rectangle(0.4, 0.5, 0.4, 0.5);
     }
 }
--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,7 +1,10 @@
 package game;
+import game.BaseMap;
     
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        BaseMap map = new BaseMap();
+        map.drawGameInfos();
     }
 }
