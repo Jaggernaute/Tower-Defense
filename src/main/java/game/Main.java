8eb15ad05325bc33d083ffe15af2021f998e995e
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2024-11-30 18:55:07 +0100

    Modify Method Map.drawPlayerInfos

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -4,11 +4,12 @@ import java.awt.Font;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Map map = new Map(0.0);
-        int[] level = {0, 0};
-        int[] waves = {0,0};
+        Font font = new Font("Arial", Font.BOLD, 13);
+        Map map = new Map(0.0, font);
+        int[] level = {2, 4};
+        int[] waves = {1,6};
         map.drawGameInfos(level, waves);
-        map.drawPlayerInfo();
+        map.drawPlayerInfos(50, 20);
         map.drawStore();
         map.drawMainMap();
     }
--- src/main/java/game/Map.java
+++ src/main/java/game/Map.java
@@ -4,18 +4,19 @@ import java.awt.Font;
 
 public class Map {
     double version;
+    Font font;
     
     //Setup de la fenetre
-    public Map(double ver){
+    public Map(double ver, Font font){
         this.version = ver;
+        this.font = font;
         StdDraw.setCanvasSize(1080,720);
         StdDraw.setTitle("Tower Defense VideCoq_Merrer V" + version);
     }
 
     //Dessine la zone Info Jeu
     public void drawGameInfos(int[] level, int[] waves){
-        Font font = new Font("Arial", Font.BOLD, 13);
-        StdDraw.setFont(font);
+        StdDraw.setFont(this.font);
         StdDraw.text(0.85, 0.985, "LVL:" +level[0]+"/" + level[1]);
         StdDraw.text(0.95, 0.985, "WAVE:" + waves[0] + "/" + waves[1]);
         
@@ -26,11 +27,25 @@ public class Map {
     }
 
     //Dessine la zone Info Joueur
-    public void drawPlayerInfo(){
+    public void drawPlayerInfos(int coinsValue, int lifeValue){
         
+        this.drawCoinIcon();
+        Font f = new Font("Arial", Font.BOLD, 20);
+        StdDraw.setFont(f);
+        StdDraw.setPenColor(StdDraw.YELLOW);
+        StdDraw.text(0.85, 0.945, "" + coinsValue);
         StdDraw.setPenRadius(0.003);
         StdDraw.setPenColor(StdDraw.GREEN);
         StdDraw.rectangle(0.9, 0.947, 0.1, 0.023);
+    }
+
+    private void drawCoinIcon(){
+        StdDraw.setPenColor(StdDraw.YELLOW);
+        StdDraw.setPenRadius(0.05);
+        StdDraw.point(0.82, 0.947);
+        StdDraw.setPenColor(StdDraw.SILVER);
+        StdDraw.setPenRadius(0.04);
+        StdDraw.point(0.82, 0.947);
     }
 
     //Dessine la zone Boutique
@@ -43,7 +58,7 @@ public class Map {
     //Dessine la zone Carte
     public void drawMainMap(){
         StdDraw.setPenRadius(0.003);
-        StdDraw.setPenColor(StdDraw.GREEN);
+        StdDraw.setPenColor(11, 102, 35);
         StdDraw.filledRectangle(0.4,0.5,0.4,0.5);
         StdDraw.setPenColor(StdDraw.BLACK);
         StdDraw.rectangle(0.4, 0.5, 0.4, 0.5);
