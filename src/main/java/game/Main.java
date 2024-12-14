274305e48c14d1529e6a3511e528be450a96e6df
Jaggernaute <lucas.videcoq1@gmail.com>
2024-12-14 13:25:53 +0100

    Map parsing

--- src/main/java/game/Main.java
+++ src/main/java/game/Main.java
@@ -1,20 +1,35 @@
 package game;
 
 import java.awt.Font;
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
 import std.StdAudioStereo;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] unused) {
+        LOGGER.info("Starting Tower Defense game");
         Font font = new Font("Arial", Font.BOLD, 13);
         Interface map = new Interface(0.0, font);
         int[] level = {2, 4};
-        int[] waves = {1,6};
-        StdAudioStereo.playInBackground("src/main/resources/sounds/rendertrucdefensedetour.wav");
+        int[] waves = {1, 6};
+
+        try {
+            StdAudioStereo.playInBackground("src/main/resources/sounds/rendertrucdefensedetour.wav");
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Failed to load background music file", e);
+        }
+
         map.drawGameInfos(level, waves);
         map.drawPlayerInfos(50, 20);
         map.drawShop();
         map.drawMainMap();
-        StdAudioStereo.play("src/main/resources/sounds/game_launched.wav");
+        try {
+            StdAudioStereo.play("src/main/resources/sounds/game_launched.wav");
+        } catch (Exception e) {
+            LOGGER.log(Level.WARNING, "Failed to play game launched sound", e);
+        }
     }
 }
