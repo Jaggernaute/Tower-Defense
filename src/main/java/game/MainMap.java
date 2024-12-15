8099428a5f78f595aa3d05098e9b1ecdb7784993
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2024-12-15 21:18:36 +0100

    Update MainMap.java

--- src/main/java/game/MainMap.java
+++ src/main/java/game/MainMap.java
@@ -1,21 +1,23 @@
 package game;
 
+import game.exceptions.MapException;
+
 public class MainMap {
     Case[][] mapCases;
     int offsetX;
     int offsetY;
     
-    public MainMap(String mapLink){
+    public MainMap(String mapLink) throws MapException{
         //lecture de la map et assignation des tailles sizeY et sizeX
         int sizeX = 0;
         int sizeY = 0;
         if(sizeX <= 10 && sizeY <= 10){
-            this.mapCases =  new Case[10][10];
+            this.mapCases =  new Case[sizeY][sizeX];
             this. offsetX = (10 - sizeX)/2;
             this. offsetY = (10 - sizeY)/2;
         }
         else{
-            //throw mapOutOfBoundException
+            throw new MapException("Map too big", "" , mapLink);
         }
     }
     
