2e01df686a0d57f5960af107059a6b1c5f3f7e87
Delfred353 <94049120+Delfred353@users.noreply.github.com>
2024-12-18 08:41:07 +0100

    change logger to public

--- src/main/java/game/MainMap.java
+++ src/main/java/game/MainMap.java
@@ -1,6 +1,9 @@
 package game;
 
+import java.io.*;
 import game.exceptions.MapException;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
 public class MainMap {
     Case[][] mapCases;
@@ -9,8 +12,18 @@ public class MainMap {
     
     public MainMap(String mapLink) throws MapException{
         //lecture de la map et assignation des tailles sizeY et sizeX
-        int sizeX = 0;
+        String[] lines = fileRead(mapLink);
+        if(matches(lines[0], "Error %s")){
+            
+        }
+        int sizeX = lines[0].length();
         int sizeY = 0;
+        for(String x : lines){
+            if(x != ""){
+                sizeY ++;
+            }
+        }
+        
         if(sizeX <= 10 && sizeY <= 10){
             this.mapCases =  new Case[sizeY][sizeX];
             this. offsetX = (10 - sizeX)/2;
@@ -19,6 +32,27 @@ public class MainMap {
         else{
             throw new MapException("Map too big", "" , mapLink);
         }
+    }
+
+    private String[] fileRead(String mapLink){
+        String[] lines = new String[10];
+        int i = 0;
+        try{FileReader fileReader = new FileReader(mapLink);
+            BufferedReader buffReader = new BufferedReader(fileReader);
+            while(buffReader.ready() && i <= 9){
+                lines[i] = buffReader.readLine();
+                i++;
+            }
+            if(10 <= i && buffReader.ready()){
+                lines[0] = "Error : too many lines in the file";
+                return lines;
+            }
+        }   
+        catch(Exception e){Main.LOGGER.log(Level.WARNING, "Failed to read the file : " + mapLink );
+        lines[0] = "Error : Failed to read file : " + mapLink;
+        return lines;
+    }
+        return lines;
     }
     
 }
